397f99206cc9d8465f8d91d9535a44d0ebb34298
DanielaS-Tochi <danielastochi@gmail.com>
2025-01-17 01:20:48 -0300

    Favicon and Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+
+const Header = () => {
+    return (
+        <AppBar position="static" color="primary">
+            <Toolbar>
+                <Typography variant="h6" sx={{ flexGrow: 1 }}>
+                    Soccer Flix
+                </Typography>
+                <Button color="inherit" href="/">Home</Button>
+                <Button color="inherit" href="/new-video">New Video</Button>
+            </Toolbar>
+        </AppBar>
+    );
+};
+
+export default Header;
